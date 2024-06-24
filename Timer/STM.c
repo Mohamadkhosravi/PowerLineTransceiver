0ecc4d26d412e80ded9f551a51776978b1113238
mohammad <94738811+Mohamadkhosravi@users.noreply.github.com>
2024-06-24 20:29:06 +0330

    DEV:030404: + Create & Add STM

---
+++ Timer/STM.c
@@ -0,0 +1,60 @@
+	
+#include<STM.h>
+
+	void STimerInit(void){
+	
+			
+		/*********************
+		STPAU: STM Counter Pause Control
+		0: Run
+		1: Pause
+		*/
+		_stpau=0;
+		 // Select STM Counter Clock
+	    _stck0 = STIMER_CLOCK & 1;
+	    _stck1 = (STIMER_CLOCK >> 1) & 1;
+	    _stck2 = (STIMER_CLOCK >> 2) & 1;
+		//STON: STM Counter On/Off Control
+		_ston =1;	
+		// Set STM Comparator P Match Period
+		_strp0 = STM_PERIOD & 1;
+		_strp1 = (STM_PERIOD >> 1) & 1;
+		_strp2 = (STM_PERIOD >> 2) & 1;
+		// Set STM Operating Mode
+	
+	// STM Operating Modes
+		_stio0=STM_MODE&1;
+		_stio1=(STM_MODE >> 1) & 1;
+					
+		_stio0=STM_PIN_FUNCTION&1;
+		_stio1=(STM_PIN_FUNCTION>>1)&1;
+
+		_stoc = STM_COUNTER ;
+		_stpol =OUTPUT_POLARITY;
+		_stdpx=	STM_PWM_DUTY;					
+		_stcclr = SELECT_COMPARE_MATCH;		
+			/*
+			D7~D0: STM CCRA Low Byte Register bit 7 ~ bit 0
+			STM 10-bit CCRA bit 7 ~ bit 0
+			*/
+			_stmal=0b11111111;
+			
+			/*
+			D9~D8: STM CCRA High Byte Register bit 1 ~ bit 0
+			STM 10-bit CCRA bit 9 ~ bit 8
+			*/
+			_stmah=0b11;
+	
+	        _stmae=1;
+	    	_stmaf=1;
+			
+
+		
+		
+		
+	}
+	
+	int readSTimer(void){	
+		return((_stmdl)|(_stmdh<<8));
+			
+	}
