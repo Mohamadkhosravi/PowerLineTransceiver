067eff57d7908c238fc2a590fb04586858ab795b
Mohammad <94738811+Mohamadkhosravi@users.noreply.github.com>
2024-07-24 16:11:27 +0330

    Update Timer.c

--- Timer/Timer.c
+++ Timer/Timer.c
@@ -1,16 +1,16 @@
 #include"Timer.h"
 
-#if BASE_TIMER 
 
-	void TimerBaseInit(void)
-	{
-      _pscr = PRESCALER_CLOCK_SURSE_BASE_TIMER;
-	 _tb0on = TIME_BASE0;
-	 _tb1on = TIME_BASE1;
-	 _tb0c |= TIM_BASE0_PERIOD;
-     _tb1c |= TIM_BASE1_PERIOD;
-	 InterruptEnable;
-		  
-	}
-#endif
+
+void TimerBaseInit(void)
+{
+	_pscr = PRESCALER_CLOCK_SURSE_BASE_TIMER;
+	_tb0on = TIME_BASE0;
+	_tb1on = TIME_BASE1;
+	_tb0c |= TIM_BASE0_PERIOD;
+	_tb1c |= TIM_BASE1_PERIOD;
+	InterruptEnable;
+
+}
+
 
