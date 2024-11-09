2b731293879ff187240a9169edac064dd7889e54
mohammad <94738811+Mohamadkhosravi@users.noreply.github.com>
2024-11-09 12:51:45 +0330

    DEV:030819: PLT TX with Global and local address is OK

--- Main/Main.c
+++ Main/Main.c
@@ -11,14 +11,23 @@ unsigned int temp;
 #define BUFFER_SIZE 'g' // Length of the expected string plus 1 for null termination
 char buffer[BUFFER_SIZE];
 int buffer_index = 0;
-
-
 char* UART_ReceiveString(void);
+
+unsigned int Counter;
+unsigned char Stat=0;
+int i=0;
+#define ADDRESS 'b'
+#define PUBLIC_ADDRESS 'Z'
+#define PRESSED_PUSHBUTTON  _pa0==0
+#define PERRESS  10
+unsigned char  pushButtonState=0;
+unsigned int PushButtonCounter=0;
+unsigned char AddresssValid=0;
+
+
 void main()
 {
-     unsigned int Data=0;	
-     /*unsigned int *Data;*/
-     	
+    unsigned int Data=0;		
     float  vcc=0.0;	
     S_RCC_Init();
 	ADCInit();
@@ -29,42 +38,22 @@ void main()
 	PLT1Init();
 	PLTAInit();
 	InitSmokeDetection();
-	unsigned int Counter;
-	unsigned char Stat=0;
-	int i=0;
-	#define ADDRESS 5
-	#define PUBLIC_ADDRESS 'Z'
-	#define PRESSED_PUSHBUTTON  _pa0==0
-	#define PERRESS  10
-	unsigned char  pushButtonState=0;
-	unsigned int PushButtonCounter=0;
-   unsigned char AddresssValid=0;
 
-   unsigned char *Buffr;
 	while(1)
 	{
-	/*	while(1){
-			_scc = 0b00000000;
-			_hircc=0b00000000;
-			_halt();
-			GCC_CLRWDT();
-			
-		}*/
-
-	  SmokeState Sm;
-	 
+		SmokeState Sm;
 		if (PRESSED_PUSHBUTTON) 
 		{
 			pushButtonState = 1;  // Set push button state to pressed
 			PushButtonCounter++;
 		} 	
-		else 
-		{
-			if ((pushButtonState == 1) &&( PushButtonCounter > PERRESS)) 
-			{
+		else {
+			
+			if ((pushButtonState == 1) &&( PushButtonCounter > PERRESS)) {
+				
 				PushButtonCounter=0;
 				pushButtonState=0;
-			/*	Stat=!Stat;*/
+				Stat=!Stat;
 				_pa4=~_pa4;
 				UART_Transmit('A');
 				UART_Transmit('D');
@@ -88,135 +77,69 @@ void main()
 		
 		}
 		GCC_CLRWDT();
-	Data=UART_ReceiveNonBlocking();
-        	GCC_CLRWDT();
+		Data=UART_ReceiveNonBlocking();
+		GCC_CLRWDT();
+	
 		if(Data!=0){
-		
-		if ((Data== ADDRESS) || (Data== PUBLIC_ADDRESS)) 
-		{
-		 AddresssValid=1;
-		}
-		
-		if((AddresssValid)&&(Data=='O')){
-		    UART_Transmit('A');
-			UART_Transmit('D');
-			UART_Transmit('R');
-			UART_Transmit('R');
-			UART_Transmit('E');
-			UART_Transmit('S');
-			UART_Transmit('S');
-			UART_Transmit('=');
-			UART_Transmit(ADDRESS);
-			UART_Transmit(' ');
-			UART_Transmit('S');
-			UART_Transmit('T');
-			UART_Transmit('A');
-			UART_Transmit('T');
-			UART_Transmit('E');
-			UART_Transmit('=');
-			UART_Transmit('1');		
-			UART_Transmit(10);
-			AddresssValid=0;
+			
+		 	UART_Transmit(Data);
+			if ((Data== ADDRESS) || (Data== PUBLIC_ADDRESS)) {
+					
+				AddresssValid=1; 
+			 
+			}	
+			if((AddresssValid)&&(Data=='O')){
+				
+			    UART_Transmit('A');
+				UART_Transmit('D');
+				UART_Transmit('R');
+				UART_Transmit('R');
+				UART_Transmit('E');
+				UART_Transmit('S');
+				UART_Transmit('S');
+				UART_Transmit('=');
+				UART_Transmit(ADDRESS);
+				UART_Transmit(' ');
+				UART_Transmit('S');
+				UART_Transmit('T');
+				UART_Transmit('A');
+				UART_Transmit('T');
+				UART_Transmit('E');
+				UART_Transmit('=');
+				UART_Transmit('1');		
+				UART_Transmit(10);
+				AddresssValid=0;
 				_pa4=1;
+			}
+			if((AddresssValid)&&(Data=='C')){
 			
-		}
-		if((AddresssValid)&&(Data=='C')){
-			
-			UART_Transmit('A');
-			UART_Transmit('D');
-			UART_Transmit('R');
-			UART_Transmit('R');
-			UART_Transmit('E');
-			UART_Transmit('S');
-			UART_Transmit('S');
-			UART_Transmit('=');
-			UART_Transmit(ADDRESS);
-			UART_Transmit(' ');
-			UART_Transmit('S');
-			UART_Transmit('T');
-			UART_Transmit('A');
-			UART_Transmit('T');
-			UART_Transmit('E');
-			UART_Transmit('=');
-			UART_Transmit('0');		
-			UART_Transmit(10);
-			AddresssValid=0;
-			_pa4=0;
+				UART_Transmit('A');
+				UART_Transmit('D');
+				UART_Transmit('R');
+				UART_Transmit('R');
+				UART_Transmit('E');
+				UART_Transmit('S');
+				UART_Transmit('S');
+				UART_Transmit('=');
+				UART_Transmit(ADDRESS);
+				UART_Transmit(' ');
+				UART_Transmit('S');
+				UART_Transmit('T');
+				UART_Transmit('A');
+				UART_Transmit('T');
+				UART_Transmit('E');
+				UART_Transmit('=');
+				UART_Transmit('0');		
+				UART_Transmit(10);
+				AddresssValid=0;
+				_pa4=0;
 			
 			}
-		
-	    }
+			
+		}
 		
 	}
-		/*	_pa4=Stat;*/
-		/* if(Stat==0)
-		{
-	    	_pa4=1;
-	     	UART_Transmit(Stat+0x30);	
-
-		}
-		else
-		{
-	    	UART_Transmit(Stat+0x30);	
-	       _pa4=0;
-		}*/
-		 
-	/*	 
-		 
-		if(Data=='O')
-		{
-	    	_pa4=1;
-	      UART_Transmit(Data);
-
-		}
-		if(Data=='C')
-		{
-	    	UART_Transmit(Data);
-	       _pa4=0;
-		}*/
-		/*UART_Transmit(Data);*/
-	/*	Data=0;
-		for(i=0;i<=10;i++)
-		{
-		 _isgs0=~_isgen;
-		 
-		}*/
-	//	_isgen=1;
-	//	_isgs0=1;?
-	   
-	  /*  OPA_ON();
-		ISINK0_ON();*/
-		
-	    //Data=ReadADC(5);
-	 /* _vbgren=1;*/
-	
-	/*	NTC_POWER_ON;
-	    vcc=temperature(ReadADC(1), 3.0f);
-		Data=vcc*100;*/
-    //	Sm= CheckSmokeLevel(&Data);
-/*
-		if(Data<=0)Data=0;
-		GCC_DELAY(1000);
-		GCC_DELAY(1000);
-		ISINK0_OFF();
-		OPA_OFF();
-		GCC_DELAY(1000);
-		GCC_CLRWDT();
-		GCC_DELAY(20000);
-		GCC_CLRWDT();
-		GCC_DELAY(20000);
- 	    //if(Data>498)((Data/10000)%10+0x30);
-    	UART_Transmit((Data/1000)%10+0x30);
-	    UART_Transmit((Data/100)%10+0x30);
-		UART_Transmit((Data/10)%10+0x30);
-		UART_Transmit((Data)%10+0x30);
-		
-		UART_Transmit(10);
-		Counter++;*/
-		GCC_CLRWDT();
-
-	
-	
+	GCC_CLRWDT();		
 }
 
 
